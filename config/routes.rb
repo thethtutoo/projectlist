601f61b941b50c63668efddfc2e7a9704d555f52
Thet Htut Oo <tho2410@gmail.com>
2016-11-08 10:46:27 +0630

    Added test page to play with bootstrap panels

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   end
   devise_for :users
   get 'pages/home'
-    get 'about' => "pages#about"
-
+  get 'about' => "pages#about"
+  get 'test' => "pages#test"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
